0a37a0330cf63210ff8510e8be0fe75b8b71bc8b
errno <yoyo_lag@yahoo.fr>
2021-08-17 18:30:26 +0200

    loss/activation fct + interface evolution

--- NeuralNetwork/NeuralNetwork.cpp
+++ NeuralNetwork/NeuralNetwork.cpp
@@ -1,66 +1,114 @@
 #include "NeuralNetwork.h"
 #include <cmath>
+#include <cassert>
 #include <random>
 
 namespace nn
 {
-std::mt19937 _randomgenerator;
-std::uniform_real_distribution<float> _randomdistribution(-1.0, 1.0);
+std::default_random_engine _random_generator;
+std::uniform_real_distribution<float> _minus_one_one_distribution(-1.0f, 1.0f);
+std::uniform_real_distribution<float> _zero_one_distribution(0.0f, 1.0f);
 
-NeuralNetwork::NeuralNetwork(int nInputs, int nHidden, int nOutputs)
+void NeuralNetwork::randomize()
 {
-	_layers.push_back(Layer(nInputs, nHidden));
-	_layers.push_back(Layer(nHidden, nOutputs));
-	
-	
 	for (auto &layer : _layers)
 	{
-		nn::map(layer._weights, [&](nn::Matrix::value_type v) { return _randomdistribution(_randomgenerator); });
-		nn::map(layer._biases, [&](nn::Matrix::value_type v) { return _randomdistribution(_randomgenerator); });
+		nn::map(layer._weights, [&](nn::Matrix::value_type v) { return _minus_one_one_distribution(_random_generator); });
+		nn::map(layer._biases, [&](nn::Matrix::value_type v) { return _minus_one_one_distribution(_random_generator); });
 	}
 }
 
-NeuralNetwork::NeuralNetwork::Layer::Layer(int nInputs, int nOutputs) : 
+NeuralNetwork::NeuralNetwork::Layer::Layer(int nInputs, int nOutputs, ActivationFunction af) : 
 	_weights(nOutputs, nInputs), 
 	_biases(nOutputs, 1), 
-	_output(nOutputs, 1)
+	_output(nOutputs, 1), 
+	_af(af)
 {
 }
 
-void NeuralNetwork::feedforward(const std::vector<const Sample *> &samples)
+void NeuralNetwork::Layer::activate()
 {
-	nn::Matrix output;
-	output.resize(samples.front()->_target.numRows(), samples.front()->_target.numColumns());
-	
-	for (const Sample *sample : samples)
+	switch (_af)
 	{
-		feedforward(sample->_input, output);
-	}
+		case ActivationFunction::SIGMOID:
+			activation_sigmoid();
+			break;
+		
+		case ActivationFunction::SOFTMAX:
+			activation_softmax();
+			break;
+		
+		default:
+			assert(false);
+			break;
+	};
 }
 
-void NeuralNetwork::feedforward(const nn::Matrix &input)
+void NeuralNetwork::Layer::activation_sigmoid()
+{
+	nn::map(_output, [] (nn::Matrix::value_type v) { return 1.0f / (1.0f + std::expf(-v)); });
+}
+
+void NeuralNetwork::Layer::activation_softmax()
+{
+	nn::map(_output, [] (nn::Matrix::value_type v) { return std::expf(v); });
+	nn::Matrix::value_type sum = nn::sum(_output, 0.0f);
+	nn::map(_output, [&] (nn::Matrix::value_type v) { return v / sum; });
+}
+
+void NeuralNetwork::feed_forward(const nn::Matrix &input)
 {
 	const nn::Matrix *payload = &input;
 	
 	for (auto &layer : _layers)
 	{
-		nn::product(layer._weights, *payload, layer._output);
+		nn::dot(layer._weights, *payload, layer._output);
 		nn::add(layer._biases, layer._output, layer._output);
-		nn::map(layer._output, [] (nn::Matrix::value_type v) { return 1.0f / (1.0f + std::expf(-v)); });
+		layer.activate();
 		payload = &layer._output;
 	}
 }
 
-void NeuralNetwork::feedforward(const nn::Matrix &input, nn::Matrix &output)
+nn::Matrix::value_type NeuralNetwork::compute_loss(const nn::Matrix &target)
 {
-	feedforward(input);
-	nn::copy(_layers.back()._output, output);
+	switch (_lf)
+	{
+		case MEAN_SQUARE_ERROR:
+			return compute_loss_mean_square_error(target);
+		
+		case SOFTMAX_CROSS_ENTROPY:
+			return compute_loss_softmax_cross_entropy(target);
+		
+		default:
+			assert(false);
+	};
+	
+	return 0.0;
 }
 
-void NeuralNetwork::backpropagation(const nn::Matrix &input, const nn::Matrix &target)
+nn::Matrix::value_type NeuralNetwork::compute_loss_mean_square_error(const nn::Matrix &target)
+{
+	nn::Matrix::value_type v = (nn::Matrix::value_type)0.0;
+	nn::map(_layers.back()._output, target, [&] (nn::Matrix::value_type a, nn::Matrix::value_type b) {
+		v += (a - b) * (a - b);
+	});
+	v /= (target.numRows() * target.numColumns());
+	return v;
+}
+
+nn::Matrix::value_type NeuralNetwork::compute_loss_softmax_cross_entropy(const nn::Matrix &target)
+{
+	nn::Matrix::value_type v = (nn::Matrix::value_type)0.0;
+	nn::map(_layers.back()._output, target, [&] (nn::Matrix::value_type a, nn::Matrix::value_type b) {
+		v += a * std::log(b);
+	});
+	return -v;
+}
+
+void NeuralNetwork::back_propagation(const nn::Matrix &input, const nn::Matrix &target)
 {
 	/* nn::Matrix guess(target.numRows(), target.numColumns());
-	feedforward(input, guess);
+	feed_forward(input, guess);
 	
 	nn::Matrix error(target.numRows(), target.numColumns());
 	nn::subtract(target, guess, error);
@@ -69,6 +117,41 @@ void NeuralNetwork::backpropagation(const nn::Matrix &input, const nn::Matrix &t
 	{
 		nn::transpose(layer._weights, layer._tweights);
 	} */
+}
+
+void NeuralNetwork::mutate(double rate)
+{
+	// printf("NeuralNetwork::mutate(%f): ", rate);
+	int nmutations = 0, nvalues = 0;
+	
+	for (Layer &layer : _layers)
+	{
+		nn::map(layer._weights, [&] (nn::Matrix::value_type v) {
+			float p = _zero_one_distribution(_random_generator);
+			if (p <= rate)
+			{
+				// v += 0.5f * _minus_one_one_distribution(_random_generator);
+				v = _minus_one_one_distribution(_random_generator);
+				nmutations += 1;
+			}
+			return v;
+		});
+		nvalues += layer._weights.numRows() * layer._weights.numColumns();
+		
+		nn::map(layer._biases, [&] (nn::Matrix::value_type v) {
+			float p = _zero_one_distribution(_random_generator);
+			if (p <= rate)
+			{
+				// v += 0.5f * _minus_one_one_distribution(_random_generator);
+				v = _minus_one_one_distribution(_random_generator);
+				nmutations += 1;
+			}
+			return v;
+		});
+		nvalues += layer._biases.numRows() * layer._biases.numColumns();
+	}
+	
+	// printf("%d / %d (%f)\n", nmutations, nvalues, (double)nmutations / (double)nvalues);
 }
 
 }; // namespace nn
--- NeuralNetwork/NeuralNetwork.h
+++ NeuralNetwork/NeuralNetwork.h
@@ -2,16 +2,27 @@
 #define __NN_NEURAL_NETWORK_H__
 
 #include "Matrix.h"
+#include <initializer_list>
 #include <vector>
 
 namespace nn
 {
 
+enum class ActivationFunction
+{
+	SIGMOID, 
+	SOFTMAX
+};
+
+enum class LossFunction
+{
+	MEAN_SQUARE_ERROR, 
+	SOFTMAX_CROSS_ENTROPY
+};
+
 class NeuralNetwork
 {
 public:
-	NeuralNetwork(int nInputs, int nHidden, int nOutputs);
-	
 	struct Sample
 	{
 		nn::Matrix _input;
@@ -23,21 +34,52 @@ public:
 		nn::Matrix _weights;
 		nn::Matrix _biases;
 		nn::Matrix _output;
+		ActivationFunction _af;
 		
-		Layer(int nInputs, int nOutputs);
+		Layer(int nInputs, int nOutputs, ActivationFunction af);
+		
+		void activate();
+		void activation_sigmoid();
+		void activation_softmax();
 	};
+	
+	struct LayerInfo
+	{
+		int units;
+		ActivationFunction af;
+	};
+	
+	NeuralNetwork(int nInputs, std::initializer_list<LayerInfo> layers, LossFunction lf)
+	{
+		for (std::initializer_list<LayerInfo>::iterator it = layers.begin(); it != layers.end(); ++it)
+		{
+			_layers.push_back(Layer(nInputs, it->units, it->af));
+			nInputs = it->units;
+		}
+		
+		_lf = lf;
+		
+		randomize();
+	}
 	
 	using LayerList = std::vector<Layer>;
 	const LayerList &layers() const { return _layers; }
 	
-	void feedforward(const std::vector<const Sample *> &samples);
-	void feedforward(const nn::Matrix &input);
-	void feedforward(const nn::Matrix &input, nn::Matrix &output);
+	void randomize();
 	
-	void backpropagation(const nn::Matrix &input, const nn::Matrix &target);
+	void feed_forward(const nn::Matrix &input);
+	
+	nn::Matrix::value_type compute_loss(const nn::Matrix &target);
+	nn::Matrix::value_type compute_loss_mean_square_error(const nn::Matrix &target);
+	nn::Matrix::value_type compute_loss_softmax_cross_entropy(const nn::Matrix &target);
+	
+	void back_propagation(const nn::Matrix &input, const nn::Matrix &target);
+	
+	void mutate(double rate);
 	
 protected:
 	LayerList _layers;
+	LossFunction _lf;
 };
 
 }; // namespace nn
